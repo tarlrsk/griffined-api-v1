ccff0ff716551e711b7bcd10296e04a8d9fbe824
tarlrsk <thanatuch.2003@gmail.com>
2023-06-19 12:34:34 +0700

    add master role to every api path

---
+++ Dtos/UserDtos/tokenDtoResponseDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace griffined_api.Dtos.UserDtos
+{
+    public class tokenDtoResponseDto
+    {
+        public string accessToken { get; set; } = string.Empty;
+    }
+}
