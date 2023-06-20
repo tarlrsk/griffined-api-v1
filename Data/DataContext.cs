af34770e07ee5a4bf316359ed5b12d0ace8e4fcd
Pan Toprasert <u6316801@au.edu>
2023-06-20 16:52:44 +0700

    refactor datacontext and delete all migrations

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+#nullable disable
+
 namespace griffined_api.Data
 {
     public class DataContext : Microsoft.EntityFrameworkCore.DbContext
@@ -12,36 +14,36 @@ namespace griffined_api.Data
 
         }
 
-        public DbSet<Address> Addresses => Set<Address>();
-        public DbSet<Appointment> Appointments => Set<Appointment>();
-        public DbSet<AppointmentMember> AppointmentMembers => Set<AppointmentMember>();
-        public DbSet<CancellationRequest> CancellationRequests => Set<CancellationRequest>();
-        public DbSet<Course> Courses => Set<Course>();
-        public DbSet<CourseMember> CourseMembers => Set<CourseMember>();
-        public DbSet<Level> Levels => Set<Level>();
-        public DbSet<NewCourseRequest> NewCourseRequests => Set<NewCourseRequest>();
-        public DbSet<NewCourseSubjectRequest> NewCourseSubjectRequests => Set<NewCourseSubjectRequest>();
-        public DbSet<Parent> Parents => Set<Parent>();
-        public DbSet<PreferredDay> PreferredDays => Set<PreferredDay>();
-        public DbSet<PreferredDayRequest> PreferredDayRequests => Set<PreferredDayRequest>();
-        public DbSet<RegistrationRequest> RegistrationRequests => Set<RegistrationRequest>();
-        public DbSet<RegistrationRequestMember> RegistrationRequestMembers => Set<RegistrationRequestMember>();
-        public DbSet<Schedule> Schedules => Set<Schedule>();
-        public DbSet<Staff> Staff => Set<Staff>();
-        public DbSet<StaffNotification> StaffNotifications => Set<StaffNotification>();
-        public DbSet<Student> Students => Set<Student>();
-        public DbSet<StudentAddingRequest> StudentAddingRequests => Set<StudentAddingRequest>();
-        public DbSet<StudentAdditionalFile> StudentAdditionalFiles => Set<StudentAdditionalFile>();
-        public DbSet<StudentAttendance> StudentAttendances => Set<StudentAttendance>();
-        public DbSet<StudentNotification> StudentNotifications => Set<StudentNotification>();
-        public DbSet<StudentReport> StudentReports => Set<StudentReport>();
-        public DbSet<StudyClass> StudyClasses => Set<StudyClass>();
-        public DbSet<StudyCourse> StudyCourses => Set<StudyCourse>();
-        public DbSet<StudyCourseHistory> StudyCourseHistories => Set<StudyCourseHistory>();
-        public DbSet<StudySubject> StudySubjects => Set<StudySubject>();
-        public DbSet<Subject> Subjects => Set<Subject>();
-        public DbSet<Teacher> Teachers => Set<Teacher>();
-        public DbSet<TeacherNotification> TeacherNotifications => Set<TeacherNotification>();
-        public DbSet<WorkTime> WorkTimes => Set<WorkTime>();
+        public virtual DbSet<Address> Addresses { get; set; }
+        public virtual DbSet<Appointment> Appointments { get; set; }
+        public virtual DbSet<AppointmentMember> AppointmentMembers { get; set; }
+        public virtual DbSet<CancellationRequest> CancellationRequests { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<CourseMember> CourseMembers { get; set; }
+        public virtual DbSet<Level> Levels { get; set; }
+        public virtual DbSet<NewCourseRequest> NewCourseRequests { get; set; }
+        public virtual DbSet<NewCourseSubjectRequest> NewCourseSubjectRequests { get; set; }
+        public virtual DbSet<Parent> Parents { get; set; }
+        public virtual DbSet<PreferredDay> PreferredDays { get; set; }
+        public virtual DbSet<PreferredDayRequest> PreferredDayRequests { get; set; }
+        public virtual DbSet<RegistrationRequest> RegistrationRequests { get; set; }
+        public virtual DbSet<RegistrationRequestMember> RegistrationRequestMembers { get; set; }
+        public virtual DbSet<Schedule> Schedules { get; set; }
+        public virtual DbSet<Staff> Staff { get; set; }
+        public virtual DbSet<StaffNotification> StaffNotifications { get; set; }
+        public virtual DbSet<Student> Students { get; set; }
+        public virtual DbSet<StudentAddingRequest> StudentAddingRequests { get; set; }
+        public virtual DbSet<StudentAdditionalFile> StudentAdditionalFiles { get; set; }
+        public virtual DbSet<StudentAttendance> StudentAttendances { get; set; }
+        public virtual DbSet<StudentNotification> StudentNotifications { get; set; }
+        public virtual DbSet<StudentReport> StudentReports { get; set; }
+        public virtual DbSet<StudyClass> StudyClasses { get; set; }
+        public virtual DbSet<StudyCourse> StudyCourses { get; set; }
+        public virtual DbSet<StudyCourseHistory> StudyCourseHistories { get; set; }
+        public virtual DbSet<StudySubject> StudySubjects { get; set; }
+        public virtual DbSet<Subject> Subjects { get; set; }
+        public virtual DbSet<Teacher> Teachers { get; set; }
+        public virtual DbSet<TeacherNotification> TeacherNotifications { get; set; }
+        public virtual DbSet<WorkTime> WorkTimes { get; set; }
     }
 }
