363a96a7f57368aec9a365ae079a47339ca36a28
Pan <97938472+Pan-1245@users.noreply.github.com>
2023-07-30 20:52:11 +0700

    implemented all extension methods for DateTime and TimeOnly

--- Extensions/DateTimeExtensions.cs
+++ Extensions/DateTimeExtensions.cs
@@ -5,20 +5,52 @@ namespace Extensions.DateTimeExtensions
 {
     public static class DateTimeExtensions
     {
-        // Extension method for convert DateTime to String.
-        public static string ToFormattedString(this DateTime dateTime, string format = "dd-MMMM-yyyy")
+        // Extension method to convert DateTime to String (format: dd-MMMM-yyyy).
+        public static string ToDateString(this DateTime dateTime, string format = "dd-MMMM-yyyy")
         {
             return dateTime.ToString(format, CultureInfo.InvariantCulture);
         }
 
-        // Extension method to covert String to DateTime. This method returns null if parsing is failed.
-        public static DateTime? ToDate(this string dateString, string format = "dd-MMMM-yyyy")
+        // Extension method to convert TimeOnly to String (format: HH:mm:ss)
+        public static string ToTimeString(this TimeOnly time, string format = "HH:mm:ss")
         {
-            if (DateTime.TryParseExact(dateString, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime result))
+            return time.ToString(format, CultureInfo.InvariantCulture);
+        }
+
+        // Extension method to convert DateTime to String (format: dd-MMMM-yyyy HH:mm:ss).
+        public static string ToDateTimeString(this DateTime dateTime, string format = "dd-MMMM-yy HH:mm:ss")
+        {
+            return dateTime.ToString(format, CultureInfo.InvariantCulture);
+        }
+
+        // Extension method to covert String to DateTime. This method throws an argument exception when the date format or date string is invalid.
+        public static DateTime ToDateTime(this string dateString)
+        {
+            DateTime result;
+
+            if (DateTime.TryParse(dateString, out result))
             {
                 return result;
             }
-            return null;
+            else
+            {
+                throw new ArgumentException("Invalid date format or date string.", nameof(dateString));
+            }
+        }
+
+        // Extension method to convert String to TimeOnly. This method throws an argument exception when the time format or time string is invalid.
+        public static TimeOnly ToTime(this string timeString)
+        {
+            TimeOnly result;
+
+            if (TimeOnly.TryParse(timeString, out result))
+            {
+                return result;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid time format or time string.", nameof(timeString));
+            }
         }
     }
 }
