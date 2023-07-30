dfc77589f725d28a96ce844a7ebe2ae68b274e50
Pan <97938472+Pan-1245@users.noreply.github.com>
2023-07-30 13:46:48 +0700

    create extension methods for DateTime

---
+++ Extensions/DateTimeExtensions.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Globalization;
+
+namespace Extensions.DateTimeExtensions
+{
+    public static class DateTimeExtensions
+    {
+        // Extension method for convert DateTime to String.
+        public static string ToFormattedString(this DateTime dateTime, string format = "dd-MMMM-yyyy")
+        {
+            return dateTime.ToString(format, CultureInfo.InvariantCulture);
+        }
+
+        // Extension method to covert String to DateTime. This method returns null if parsing is failed.
+        public static DateTime? ToDateTime(this string dateString, string format = "dd-MMMM-yyyy")
+        {
+            if (DateTime.TryParseExact(dateString, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime result))
+            {
+                return result;
+            }
+            return null;
+        }
+    }
+}
