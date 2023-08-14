b0e8022e29a33a4e4215bbd585934077477d7796
Pan <97938472+Pan-1245@users.noreply.github.com>
2023-08-14 12:28:54 +0700

    throw new BadRequestException if there is an existing report with same progression

--- Services/StudentReportService/StudentReportService.cs
+++ Services/StudentReportService/StudentReportService.cs
@@ -40,6 +40,11 @@ namespace griffined_api.Services.StudentReportService
 
             int teacherId = _firebaseService.GetAzureIdWithToken();
 
+            var existingReport = dbMember.StudentReports.FirstOrDefault(sr => sr.Progression == progression);
+
+            if (existingReport != null)
+                throw new BadRequestException("Report already existed");
+
             if (fileToUpload != null)
             {
                 var studentReport = new StudentReport();
