6a3c04b7b4cb4fd70f89e1c7dafa64a32ea83210
tarlrsk <thanatuch.2003@gmail.com>
2023-08-16 10:44:36 +0700

    organize dto

--- Dtos/StudyCourseDtos/EnrolledStudyCourseResponseDto.cs
+++ Dtos/StudyCourseDtos/EnrolledStudyCourseResponseDto.cs
@@ -7,12 +7,12 @@ namespace griffined_api.Dtos.StudyCourseDtos
 {
     public class EnrolledStudyCourseResponseDto
     {
-        public string Section { get; set; } = string.Empty;
         public int StudyCourseId { get; set; }
         public string Course { get; set; } = string.Empty;
         public List<StudySubjectResponseDto> StudySubjects { get; set; } = new List<StudySubjectResponseDto>();
         public StudyCourseType StudyCourseType { get; set; }
         public int? LevelId { get; set; }
         public string? Level { get; set; }
+        public string Section { get; set; } = string.Empty;
     }
 }
