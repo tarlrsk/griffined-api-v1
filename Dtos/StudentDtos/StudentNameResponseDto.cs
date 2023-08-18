7bc5596c3b597223b1250e49270bba657f2454c0
tarlrsk <thanatuch.2003@gmail.com>
2023-08-18 12:30:02 +0700

    add member in list studentCourse

--- Dtos/StudentDtos/StudentNameResponseDto.cs
+++ Dtos/StudentDtos/StudentNameResponseDto.cs
@@ -9,6 +9,8 @@ namespace griffined_api.Dtos.StudentDtos
     {
         public int StudentId { get; set; }
         public string StudentCode { get; set; } = string.Empty;
+        public string FirstName {get; set;} = string.Empty;
+        public string LastName {get; set;} = string.Empty;
         public string FullName { get; set; } = string.Empty;
         public string Nickname { get; set; } = string.Empty;
     }
--- Dtos/StudyCourseDtos/StudyCourseResponseDto.cs
+++ Dtos/StudyCourseDtos/StudyCourseResponseDto.cs
@@ -16,6 +16,7 @@ namespace griffined_api.Dtos.StudyCourseDtos
         public string StartDate { get; set; } = string.Empty;
         public string EndDate { get; set; } = string.Empty;
         public Method Method { get; set; }
+        public List<StudentNameResponseDto> members {get; set;} = new List<StudentNameResponseDto>();
         public List<StudySubjectResponseDto> StudySubjects { get; set; } = new List<StudySubjectResponseDto>();
         public StudyCourseType StudyCourseType { get; set; }
         public CourseStatus CourseStatus { get; set; }
