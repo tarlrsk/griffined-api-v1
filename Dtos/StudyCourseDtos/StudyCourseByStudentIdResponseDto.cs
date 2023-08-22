bb9d7a66100eed052ad839f24017cab8cda53c82
Pan Toprasert <u6316801@au.edu>
2023-08-22 11:11:40 +0700

    crete dto for GetStudyCoursesByStudentId

---
+++ Dtos/StudyCourseDtos/StudyCourseByStudentIdResponseDto.cs
@@ -0,0 +1,18 @@
+using griffined_api.Dtos.StudentReportDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace griffined_api.Dtos.StudyCourseDtos
+{
+    public class StudyCourseByStudentIdResponseDto
+    {
+        public int StudentId { get; set; }
+        public string StudentCode { get; set; } = string.Empty;
+        public int CourseId { get; set; }
+        public string Course { get; set; } = string.Empty;
+        public StudyCourseStatus Status { get; set; }
+        public List<StudySubjectReportResponseDto> Reports = new List<StudySubjectReportResponseDto>();
+    }
+}
