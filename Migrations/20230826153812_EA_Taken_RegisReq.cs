bb72168f035ae81ce990f73d98f70ef3176f1f4f
tarlrsk <thanatuch.2003@gmail.com>
2023-08-26 22:38:30 +0700

    migrate ea taken regis req

---
+++ Migrations/20230826153812_EA_Taken_RegisReq.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace griffinedapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class EATakenRegisReq : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TakenByEAId",
+                table: "RegistrationRequest",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TakenByEAId",
+                table: "RegistrationRequest");
+        }
+    }
+}
