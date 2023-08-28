1da77a8c0c14fe65ab4ba3fb0f61842cc0df9e1f
Pan Toprasert <u6316801@au.edu>
2023-08-28 09:35:06 +0700

    change branch for fixing authorization for GetAllStudyCourse

--- Controllers/StudyCourseController.cs
+++ Controllers/StudyCourseController.cs
@@ -24,7 +24,7 @@ namespace griffined_api.Controllers
             return Ok(await _studyCourseService.AddGroupSchedule(newRequestedSchedule));
         }
 
-        [HttpGet(), Authorize(Roles = "ec, ea, oa, master")]
+        [HttpGet(), Authorize(Roles = "ec, oa, master")]
         public async Task<ActionResult> GetAllStudyCourse()
         {
             return Ok(await _studyCourseService.GetAllStudyCourse());
