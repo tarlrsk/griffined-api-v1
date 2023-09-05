d3e37af65ee690001703119f5a8aaa0872f489bc
Pan <97938472+Pan-1245@users.noreply.github.com>
2023-09-05 20:09:49 +0700

    fix warning

--- Dtos/StudyCourseDtos/StudyCourseHistoryResponseDto.cs
+++ Dtos/StudyCourseDtos/StudyCourseHistoryResponseDto.cs
@@ -7,7 +7,7 @@ namespace griffined_api.Dtos.StudyCourseDtos
 {
     public class StudyCourseHistoryResponseDto
     {
-        public string Date { get; set; }
+        public string Date { get; set; } = string.Empty;
         public StudyCourseHistoryType RecordType { get; set; }
         public string Record { get; set; } = string.Empty;
     }
