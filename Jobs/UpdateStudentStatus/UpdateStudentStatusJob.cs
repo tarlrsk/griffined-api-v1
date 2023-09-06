1d1124324461d77a8d3bf7c8d75c2bbbe1c936f0
Pan <97938472+Pan-1245@users.noreply.github.com>
2023-09-06 22:05:39 +0700

    implement cron for updating student status active -> inactive

---
+++ Jobs/UpdateStudentStatus/UpdateStudentStatusJob.cs
@@ -0,0 +1,28 @@
+using Quartz;
+
+namespace griffined_api.Jobs.UpdateStudentStatus
+{
+    [DisallowConcurrentExecution]
+    public class UpdateStudentStatusJob : IJob
+    {
+        private readonly DataContext _context;
+        public UpdateStudentStatusJob(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task Execute(IJobExecutionContext context)
+        {
+            var dbStudents = await _context.Students.ToListAsync();
+
+            foreach (var dbStudent in dbStudents)
+            {
+                if (DateTime.Now > dbStudent.ExpiryDate)
+                {
+                    dbStudent.Status = StudentStatus.Inactive;
+                }
+            }
+
+            await Task.CompletedTask;
+        }
+    }
+}
