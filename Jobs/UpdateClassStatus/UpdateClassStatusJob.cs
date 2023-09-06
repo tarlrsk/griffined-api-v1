68876f0539a5199f1d37e681e26866b84ab23a15
Pan Toprasert <u6316801@au.edu>
2023-09-06 10:33:25 +0700

    implement logic

--- Jobs/UpdateClassStatus/UpdateClassStatusJob.cs
+++ Jobs/UpdateClassStatus/UpdateClassStatusJob.cs
@@ -5,16 +5,37 @@ namespace griffined_api.Jobs.UpdateClassStatus
     [DisallowConcurrentExecution]
     public class UpdateClassStatusJob : IJob
     {
+        private readonly DataContext _context;
         private readonly ILogger<UpdateClassStatusJob> _logger;
-        public UpdateClassStatusJob(ILogger<UpdateClassStatusJob> logger)
+        public UpdateClassStatusJob(DataContext context, ILogger<UpdateClassStatusJob> logger)
         {
+            _context = context;
             _logger = logger;
         }
 
-        public Task Execute(IJobExecutionContext context)
+        public async Task Execute(IJobExecutionContext context)
         {
-            _logger.LogInformation($"Job executed at: {DateTime.Now}");
-            return Task.CompletedTask;
+            var dbStudyClasses = await _context.StudyClasses.ToListAsync();
+
+            foreach (var dbStudyClass in dbStudyClasses)
+            {
+                var classEndTime = dbStudyClass.Schedule.Date.Add(dbStudyClass.Schedule.ToTime);
+
+                if (dbStudyClass.Status == ClassStatus.Checked)
+                {
+                    _logger.LogInformation($"No changes at {DateTime.Now}");
+                    continue;
+                }
+
+                if (DateTime.Now > classEndTime)
+                {
+                    dbStudyClass.Status = ClassStatus.Unchecked;
+                    _logger.LogInformation($"Class Status changed to Unchecked at {DateTime.Now}");
+                    await _context.SaveChangesAsync();
+                }
+            }
+
+            await Task.CompletedTask;
         }
     }
 }
