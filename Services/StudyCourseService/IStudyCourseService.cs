b174fb6bf70322434416a1f4a3f44d77446aa4a9
tarlrsk <thanatuch.2003@gmail.com>
2023-09-08 21:52:45 +0700

    add cancel study course

--- Services/StudyCourseService/IStudyCourseService.cs
+++ Services/StudyCourseService/IStudyCourseService.cs
@@ -28,5 +28,6 @@ namespace griffined_api.Services.StudyCourseService
         Task<ServiceResponse<ClassProgressResponseDto>> GetCourseProgress(int studyCourseId);
         Task<ServiceResponse<List<TodayClassMobileResponse>>> GetMobileTodayClass(string date);
         Task<ServiceResponse<List<StudyCourseHistoryResponseDto>>> GetStudyCourseHistory(int studyCourseId);
+        Task<ServiceResponse<string>> CancelStudyCourse(int studyCourseId);
     }
 }
