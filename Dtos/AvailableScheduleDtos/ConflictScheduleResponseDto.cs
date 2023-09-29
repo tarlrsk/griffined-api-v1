4286a5118a5c52b29807ac9b15e181eb2ac19525
Thanatuch Lertritsirikul <thanatuch.2003@gmail.com>
2023-09-30 00:33:43 +0700

    remove unused import2

--- Dtos/AvailableScheduleDtos/ConflictScheduleResponseDto.cs
+++ Dtos/AvailableScheduleDtos/ConflictScheduleResponseDto.cs
@@ -2,10 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Google.Type;
-using Microsoft.AspNetCore.Http.HttpResults;
-using Newtonsoft.Json;
-
 namespace griffined_api.Dtos.AvailableScheduleDtos
 {
     public class ConflictScheduleResponseDto
