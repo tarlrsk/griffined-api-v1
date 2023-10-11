d0b44f074fd0f215645b3d6ece8a21c8b8fd7ac2
Pan1245 <pan.1245.dev@gmail.com>
2023-10-11 11:39:17 +0700

    remove unnecessary using directives

--- Controllers/NotificationController.cs
+++ Controllers/NotificationController.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
-using griffined_api.Services.NotificationService;
-using Microsoft.OpenApi.Writers;
 
 namespace griffined_api.Controllers
 {
