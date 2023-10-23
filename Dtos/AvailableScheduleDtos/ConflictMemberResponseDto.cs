2f6b7fdf76309824def93e0da9cb8db53baeb4a2
Thanatuch Lertritsirikul <thanatuch.2003@gmail.com>
2023-10-23 19:18:49 +0700

    fix response

--- Dtos/AvailableScheduleDtos/ConflictMemberResponseDto.cs
+++ Dtos/AvailableScheduleDtos/ConflictMemberResponseDto.cs
@@ -8,6 +8,7 @@ namespace griffined_api.Dtos.AvailableScheduleDtos
     public class ConflictMemberResponseDto
     {
         public string Role { get; set; } = string.Empty;
+        public int MemberId { get; set; }
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string FullName { get; set; } = string.Empty;
