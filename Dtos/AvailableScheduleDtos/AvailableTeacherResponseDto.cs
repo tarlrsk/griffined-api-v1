4ada1565d681363442263a6a90d0d126e3b85eac
Thanatuch Lertritsirikul <thanatuch.2003@gmail.com>
2023-10-23 20:06:24 +0700

    include conflict teacher

--- Dtos/AvailableScheduleDtos/AvailableTeacherResponseDto.cs
+++ Dtos/AvailableScheduleDtos/AvailableTeacherResponseDto.cs
@@ -7,12 +7,13 @@ namespace griffined_api.Dtos.AvailableScheduleDtos
 {
     public class AvailableTeacherResponseDto
     {
+        public bool IsConflict { get; set; }
         public int TeacherId { get; set; }
         public string FirebaseId { get; set; } = string.Empty;
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string FullName { get; set; } = string.Empty;
         public string Nickname { get; set; } = string.Empty;
-        public TeacherWorkType TeacherWorkType { get; set; }
+        public List<ConflictScheduleResponseDto> ConflictSchedules { get; set; } = new List<ConflictScheduleResponseDto>();
     }
 }
