1f390aba588575866f6f4b730fcde0ae776feb98
Pan <siwach.toprasert@gmail.com>
2024-05-18 15:00:36 +0700

    migrations: update schedule hierarchy

--- Models/Schedule.cs
+++ Models/Schedule.cs
@@ -7,18 +7,29 @@ using System.Threading.Tasks;
 
 namespace griffined_api.Models
 {
+    [Table("Schedules")]
     [Index(nameof(Date))]
     public class Schedule
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         public DateTime Date { get; set; }
+
         public TimeSpan FromTime { get; set; }
+
         public TimeSpan ToTime { get; set; }
 
         public virtual ScheduleType Type { get; set; }
+
+        [Column("nvarchar(100)")]
+        public virtual DailyCalendarType CalendarType { get; set; }
+
         public virtual StudyClass? StudyClass { get; set; }
+
         public virtual AppointmentSlot? AppointmentSlot { get; set; }
+
         public string? Room { get; set; }
     }
 }
